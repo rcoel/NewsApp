86d2c5d5e8e81d1bb2cc605d76ef96a6df6ec863
[Reevan Coelho] <reevancoelho@gmail.com>
2023-03-28 05:54:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //1fe9137d8c2049c2b80279081d305a0c
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
