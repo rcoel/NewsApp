16e6ba1196cae084cf86b5283209ab0a40541bcd
[Reevan Coelho] <reevancoelho@gmail.com>
2023-07-06 11:55:25 +0530

    UI changes

--- app/src/main/java/com/example/newsapp/EditProfileActivity.java
+++ app/src/main/java/com/example/newsapp/EditProfileActivity.java
@@ -1,17 +1,27 @@
 package com.example.newsapp;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class EditProfileActivity extends AppCompatActivity {
 
@@ -19,6 +29,12 @@ public class EditProfileActivity extends AppCompatActivity {
     Button saveButton;
     String nameUser, usernameUser,userId;
     DatabaseReference reference;
+
+    FloatingActionButton profileBtn;
+
+    ShapeableImageView img;
+
+    ActivityResultLauncher<Intent> imagePickerLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +47,33 @@ public class EditProfileActivity extends AppCompatActivity {
         editUsername = findViewById(R.id.editUsername);
         saveButton = findViewById(R.id.saveButton);
 
+        profileBtn = findViewById(R.id.profileBtn);
+        img = findViewById(R.id.profileImg);
+
         showData();
 
+        imagePickerLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                        Uri imageUri = result.getData().getData();
+                        // Display the selected image
+                        Glide.with(this)
+                                .load(imageUri)
+                                .apply(RequestOptions.circleCropTransform())
+                                .into(img);
+                    }
+                });
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                imagePickerLauncher.launch(intent);
+            }
+        });
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (isNameChanged() || isUsernameChanged()) {
+                if (isNameChanged() || isUsernameChanged() || isProfileChanged()) {
                     Toast.makeText(EditProfileActivity.this, "Saved", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(EditProfileActivity.this, "No Changes Found", Toast.LENGTH_SHORT).show();
@@ -46,6 +83,12 @@ public class EditProfileActivity extends AppCompatActivity {
                 startActivity(it);
             }
         });
+    }
+
+
+
+    private boolean isProfileChanged() {
+        return false;
     }
 
     private boolean isUsernameChanged() {
